f7268519f53c75227ed4afba97c005c232247164
Yashmeet <your-github-email@gmail.com>
2026-07-08 11:24:35 +0530

    Update API URL to production and fix CORS

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const flashcardRoutes = require('./routes/flashcardRoutes');
 const mistakeRoutes = require('./routes/mistakeRoutes');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
